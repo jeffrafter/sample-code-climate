fba919cf9be9a08c224d113f9c883dc4db4d8a17
Jeff Rafter <jeffrafter@gmail.com>
2014-12-08 09:35:25 -0800

    Move it to a method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_action do
+    permit_params
+  end
+
+  private
+
+  def permit_params
     params.permit!
   end
 end
